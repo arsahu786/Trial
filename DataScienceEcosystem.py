eb1b6451390308a1034375eea53b14c4767149da
arsahu786 <130399557+arsahu786@users.noreply.github.com>
2023-04-11 00:28:50 +0530

    Data Science and its tools - Assignment
    Contains Assignment

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized
+
+# Some of the popular languages that Data Scientists use are:
+#     1. Python
+#     2. R
+#     3. SQL
+#     4. Java
+
+# Some of the commonly used libraries used by Data Scientists include:
+#     1. Pandas
+#     2. Numpy
+#     3. Scipy
+#     4. Scikit learn
+
+# | Data Science Tools |
+# |:-------------|
+# | Jupyter Notebooks |
+# | R Studio’s  |
+# | Google Colab |
+
+# # Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[3]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+hour=60
+hours=200/60
+hours
+
+
+# __Objectives:__
+#     * Most Popular 3 language used in data science are Python, R and SQL.
+#     * Most Popular Python Libtraries are Pandas, Numpy, Scipy, matplotlib and Scikitlearn.
+#     * Jupyter Notebook and GoogleColab are two famous open source tools for data science.
+#     * Hadoop and Spark are two famous Bigdata open source tools.
+#     * Watson Studio is inhouse IBM based open source tool for datascience.
+
+# # Author
+# Arpan Sahu
+
+# In[ ]:
+
+
+
+
